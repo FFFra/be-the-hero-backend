ee0af9e4e973701cbbab02ad86df4a8acf9ea5a7
Flavio Fraschetti <flavio@riseup.ai>
2020-03-24 22:03:07 +0000

    implement controllers

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,20 @@ const express = require('express');
 const routes = express.Router();
 
 const ongController = require('./controllers/ongController');
-routes.post('/ongs', ongController.create);
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
 
+
+routes.post('/ongs', ongController.create);
 routes.get('/ongs', ongController.index);
+
+routes.post('/incidents', incidentController.create);
+routes.get('/incidents', incidentController.index);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
+
+routes.post('/sessions', sessionController.create);
 
 module.exports = routes;
